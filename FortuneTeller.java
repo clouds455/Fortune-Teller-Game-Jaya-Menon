1bd096b2655db356197824fe3e6fe847064829bc
clouds455 <jmenon2006@gmail.com>
2025-05-16 14:40:05 -0700

    Create FortuneTeller.java

---
+++ FortuneTeller.java
@@ -0,0 +1,23 @@
+package com.example.fortuneteller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Jaya's Fortune Teller Application");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
